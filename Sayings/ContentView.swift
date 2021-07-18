e37dd84a5d024f1f139d5818962e6c50c8223f4d
berkiyo <berk@tekbyte.net>
2021-07-18 14:13:19 +1000

    Initial commit

---
+++ Sayings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sayings
+//
+//  Created by Berk Dogan on 18/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Sayings - The definitive quotes app.")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sayings/SayingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayingsApp.swift
+//  Sayings
+//
+//  Created by Berk Dogan on 18/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SayingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
